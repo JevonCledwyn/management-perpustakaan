43c4c2d7a3c57a777176742429c9015e898223c6
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2024-11-24 02:34:07 +0700

    create user

--- app/Livewire/UserComponent.php
+++ app/Livewire/UserComponent.php
@@ -3,11 +3,37 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\User; // Tambahkan ini untuk mengimpor model User
 
 class UserComponent extends Component
 {
+    use WithPagination;
+
+    protected $themePagination='bootsrap';
+    public $nama, $email, $password;
+
     public function render()
     {
-        return view('livewire.user-component');
+        $layout['title'] = "Manage User";
+        $data['user'] = User::paginate(10); // Mengambil data user dengan pagination
+        return view('livewire.user-component', $data, $layout);
+    }
+
+    public function create(){
+        $this->validate([
+       'nama'=>'required',    
+       'email'=>'required|email',
+       'password'=>'required', 
+        ]);
+        User::create([
+            'nama' => $this->nama,
+            'email' => $this->email,
+            'password' => $this->password,
+            'jenis' => 'admin'
+        ]);
+
+        session()->flash('success','User Created');
+        $this->reset();
     }
 }
--- resources/views/livewire/user-component.blade.php
+++ resources/views/livewire/user-component.blade.php
@@ -1,3 +1,92 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <div class="card">
+        <div class="card-header">
+            Manage User
+        </div>
+        <div class="card-body">
+            @if (session()->has('success'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('success') }}
+                </div>
+            @endif
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">No.</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Jenis</th>
+                            <th>Process</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($user as $data)
+                            <tr>
+                                <th scope="row">{{ $loop->iteration }}</th>
+                                <td>{{ $data->nama }}</td>
+                                <td>{{ $data->email }}</td>
+                                <td>{{ $data->jenis }}</td>
+                                <td>
+                                    <a href="#" class="btn btn-sm btn-info">Update</a>
+                                    <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+                {{ $user->links() }}
+            </div>
+            <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#createPage">Create</a>
+        </div>
+    </div>
+
+    <!-- Create (Modal) -->
+    <div wire:ignore.self class="modal fade" id="createPage" tabindex="-1" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Create User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        {{-- Nama --}}
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" class="form-control" wire:model="nama" value="{{ @old('nama') }}">
+                            @error('nama')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        {{-- Email --}}
+                        <div class="form-group">
+                            <label for="nama">Email</label>
+                            <input type="text" class="form-control" wire:model="email" value="{{ @old('nama') }}">
+                            @error('email')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        {{-- Password --}}
+                        <div class="form-group">
+                            <label for="nama">Password</label>
+                            <input type="password" class="form-control" wire:model="password"
+                                value="{{ @old('nama') }}">
+                            @error('password')
+                                <small class="form-text text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" wire:click="create" class="btn btn-primary" data-dismiss="modal">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>

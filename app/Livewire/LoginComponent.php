7d9a718c1fe3904de593624328790e675007b796
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2024-11-24 00:45:32 +0700

    Initial commit

---
+++ app/Livewire/LoginComponent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth; // Tambahkan ini
+use Illuminate\Http\Request; // Tambahkan ini jika Anda memerlukan $request
+
+class LoginComponent extends Component
+{
+    public $email, $password;
+
+    public function render()
+    {
+        return view('livewire.login-component');
+    }
+
+    public function proses(Request $request) // Tambahkan Request sebagai parameter
+    {
+        $credentials = $this->validate([
+            'email' => 'required|email', // Tambahkan validasi format email
+            'password' => 'required',
+        ], [
+            'email.required' => 'Email must be filled!',
+            'password.required' => 'Password must be filled!',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->regenerate();
+
+            return redirect()->route('home');
+        }
+
+        return back()->withErrors([
+            'email' => 'Failed Authenticating',
+        ])->onlyInput('email');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Livewire\HomeComponent;
+use App\Livewire\LoginComponent;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',HomeComponent::class)->middleware('auth')->name('home');
+
+Route::get('login', LoginComponent::class)->name('login');
